1fd11b97d390fefe0af70a075285bc76f4de71bf
Anthony Colannino <colannia@os1.engr.oregonstate.edu>
2020-07-18 11:29:32 -0700

    working on fork

--- smallsh.c
+++ smallsh.c
@@ -2,77 +2,139 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/types.h>
 
 int main(void){
+    int kill = 1;
+    int returnStatus;
+    int exitStatus = 0;
+    int numberRuns = 0;
+
     size_t size = 0;
     char* buffer = NULL;
+    char* args[512];
 
-    printf(":");
-    fflush(stdout);
+    //Set kill = 0 when want to exit out of our shell
+    while(kill || numberRuns > 5){
 
-    //User input is held in buffer
-    getline(&buffer, &size, stdin);
+        printf(":");
+        fflush(stdout);
 
-    //Remove newline character at end of string
-    buffer[strcspn(buffer, "\n")] = 0;
+        //User input is held in buffer
+        getline(&buffer, &size, stdin);
 
-    //Tokenize command 
-    char* token1 = strtok(buffer, " ");
-    printf("Token1 length: %d\n",strlen(token1));
-    printf("Command: %s\n", token1);
+        //Remove newline character at end of string
+        buffer[strcspn(buffer, "\n")] = 0;
 
-    //need to tokenize buffer so I can check command and destination
-    if(strncmp(buffer,"cd",2) == 0){
-        //Tokenize second word of command
-        char* token2 = strtok(NULL," ");
+        //Copy input to be tokenized for arg array
+        char* argsCopy;
+        argsCopy = malloc(sizeof(char) * (strlen(buffer) + 1));
+        strcpy(argsCopy, buffer);
 
-        //There is no second word
-        //Only 'cd' read, chdir to environment variable as HOME
-        if(token2 == NULL){
-            //Set directory to home directory
-            char* homeDir = getenv("HOME");
-
-            //May need to validate this a different way
-            chdir(homeDir);
-
-            char* currentDir = getcwd(NULL, 0);
-
-            printf("current directory: %s\n", currentDir);
-            //Need to chdir to what HOME is set to as an env variable
-            printf("only cd was passed through\n");
+        //Tokenize buffer by spaces to set args array to all arguments for use in exec
+        char* argTokens = strtok(argsCopy," ");
+        int i = 0;
+        while(argTokens != NULL){
+            args[i++] = argTokens;
+            argTokens = strtok (NULL, " ");
         }
 
-        //Token 2 holds destination with cd, use token2 to chdir
-        else if (strlen(token2) > 0){
-            
-            printf("Destination: %s\n", token2);
-            printf("Token2 length: %d\n", strlen(token2));
+        //Tokenize command to be ready for built in commands
+        char* token1 = strtok(buffer, " ");
+        // COMMENT OUT THIS IS FOR MY OWN CHECKING)
+        printf("Token1 length: %d\n",strlen(token1));
+        printf("Command: %s\n", token1);
 
-            //change to directory desired
-            chdir(token2);
-            
-            //check if directory properly changed
-            char* currentDir = getcwd(NULL, 0);
-            printf("current directory: %s\n", currentDir);
+        //Tokenize buffer so I can check command and destination (MIGHT BE ABLE TO USE ARGS FOR THIS, KEEPING THIS FOR NOW)
+        if(strncmp(buffer,"cd",2) == 0){
+            //Tokenize second word of command
+            char* token2 = strtok(NULL," ");
 
-            // //This means there's a 3rd command (this might be unecessary since skipping if & is present anyways)
-            // if(token2[strlen(token2 - 1) == ' ']){
-            //     char* token3 = strtok(NULL, "\0");
-            //     //No 3rd command (looking for a &, but we don't need to do anything if present)
-            //     if(token3 == NULL){
-            //         printf("No 3rd command\n");
-            //     }
-            //     else{
-            //         printf("3rd command present\n");
-            //         printf("Mod: %s", token3);
-            //         printf("Token3 length: %d\n", strlen(token3));
-            //     }
-            // }
+            //There is no second word
+            //Only 'cd' read, chdir to environment variable as HOME
+            if(token2 == NULL){
+                //Set directory to home directory
+                char* homeDir = getenv("HOME");
+
+                //May need to validate this a different way
+                chdir(homeDir);
+
+                char* currentDir = getcwd(NULL, 0);
+
+                // COMMENT OUT THIS IS FOR MY OWN CHECKING)
+                printf("current directory: %s\n", currentDir);
+                //Need to chdir to what HOME is set to as an env variable
+                printf("only cd was passed through\n");
+            }
+
+            //Token 2 holds destination with cd, use token2 to chdir
+            else if (strlen(token2) > 0){
+                
+                // COMMENT OUT THIS IS FOR MY OWN CHECKING)
+                printf("Destination: %s\n", token2);
+                printf("Token2 length: %d\n", strlen(token2));
+
+                //change to directory desired, it should change directory in this if statement
+                if(chdir(token2) == -1){
+                    printf("Invalid destination.\n");
+                    fflush(stdout);
+                    //Exited due to error
+                    exitStatus = 1;
+                }
+                else{
+                    //Ran properly
+                    exitStatus = 0;
+                }
+                //check if directory properly changed (COMMENT OUT THIS IS FOR MY OWN CHECKING)
+                char* currentDir = getcwd(NULL, 0);
+                printf("current directory: %s\n", currentDir);
+            }
         }
+
+        //Exit built in command
+        else if (strcmp(token1,"exit") == 0){
+            kill = 0;
+            printf("Exited with an exit status of: %d\n",exitStatus);
+        }
+
+        //Status built in command
+        else if (strcmp(token1,"status") == 0){
+            printf("Exit status: %d\n",exitStatus);
+            fflush(stdout);
+        }
+
+        //Need to use exec built in function and fork (command was neither cd, exit, or status)
+        else{
+            
+            //NEED TO FIGURE OUT HOW TO REDIRECT IF THE ARRAY STARTS WITH "<" or ">"
+            //ALSO NEED TO SIGNIFY IF BACKGROUND OR FOREGROUND AS WELL
+            //ALSO NEED TO SETUP SIGNALS TO CATCH CTRL-Z and CTRL-C
+
+
+            pid_t spawnPid = -5;
+            spawnPid = fork();
+            switch(spawnPid){
+                case -1:
+                    perror("fork() failed!");
+                    exit(1);
+                    break;
+                case 0:    
+                    printf("I am the child!\n");
+                    fflush(stdout);
+                    break;
+                default:
+                    printf("I am the parent!\n");
+                    fflush(stdout);
+                    break;
+            }
+            printf("Both processes done running");
+            fflush(stdout);
+            kill = 0;
+        }
+
+        //Loop can only run 5 times (trying to save server)
+        numberRuns++;
     }
-    
-    
 
     // //Commands made by me recognized from user input --> now do work with these
     // if(strcmp(buffer, "cd")==0){
